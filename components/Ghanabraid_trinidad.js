7c25377ece70347e8615c4cb813aa7408eaffd8d
Tim Lee <systeco@hotmail.com>
2022-10-03 09:43:20 -0400

    1003-3

--- components/Ghanabraid_trinidad.js
+++ components/Ghanabraid_trinidad.js
@@ -1,54 +1,10 @@
 import React from 'react';
 import cla from '../styles/form.module.css';
-import {useRef, useState} from 'react';
-import {useRouter} from 'next/router'
 import Image from 'next/image';
 import Head from 'next/head'
 
 function Ghanabraid( ) {
-   
-    //  const refName = useRef();
-    //  const refTel = useRef();
-    //  const refEmail = useRef();
-    
-    //  const router = useRouter()
-    
-     
-    //  async function submitHandler(event) {
-    //    event.preventDefault();
-    //    const enteredName = refName.current.value;
-    //    const enteredTel = refTel.current.value;
-    //    const enteredEmail = refEmail.current.value;
-     
-      
-    //    const inputData ={
-    //      name: enteredName,
-    //      tel: enteredTel,
-    //      email: enteredEmail,
-       
-    //      date: Date(),
-    //    }
-    //    console.log(inputData);
-    //    addDataHandler(inputData)
-
-    //     document.getElementById('name').value='';
-    //     document.getElementById('tel').value='';
-    //     document.getElementById('email').value='';
-      
-    //  }
-    //  async function addDataHandler(inputData) {
-
-    //     const response = await fetch('/api/postsghana',
-    //         {
-    //             method: 'POST',
-    //             body: JSON.stringify(inputData),
-    //             headers: {
-    //                 'Content-type': 'application/json'
-    //             }
-    //         })
-    //     const data = await response.json();
-    //     router.push('/thanks')
-    // }
+  
   return (
     
     <div className={cla.main}> 
@@ -63,26 +19,7 @@ function Ghanabraid( ) {
         <meta property="og:site_name" content="Beauty Elements"/> 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head> 
-        {/* <center>
-        <Image src='https://bijouxhair.com/tim/landing2/ghanabraidhead.jpg' width='800px' height='500px' alt='banner' />
-        <div className={cla.ula2}>Fill out the Registration and Win !</div>
-      <div className={cla.inputframe}>
-      <form onSubmit={submitHandler}>
-         <input type='text' id='name' ref={refName} placeholder='Name' className={cla.inputbox} required /><br/>
-         <input type='text' id='tel' ref={refTel} placeholder='Tel' className={cla.inputbox} required /><br/>
-         <input type='text' id='email' ref={refEmail} placeholder='Email' className={cla.inputbox} required /> 
-         <button className={cla.btn}>Submit</button>
-       </form>
-       </div>
-       <div className={cla.textbox}>
-       <div className={cla.ula2}>To get the free giveaway Hair: </div>
-       <ul className={cla.ula}>
-         <li>  5 winners will be selected every 2 weeks.</li>
-         <li>  5 Packs 3x Ghana braid 50 for each Winner.</li>
-       </ul>
-       </div>
-       </center> */}
-     {/* <div className={cla.phototitle}>3X Ghana Braid Special Event</div> */}
+    
      <div className={cla.album2}>
        <div >
        <Image src='https://bijouxhair.com/tim/landing2/cutenbeauty.jpg' alt='banner' width='800px' height='400px' className={cla.image}/>
@@ -150,18 +87,7 @@ function Ghanabraid( ) {
        <div className={cla.videotitle}>3X Ghana Braid in Miami Beach</div>
        </div>
        </div>
-     {/* <div className={cla.album}>
-       <div className={cla.box} > 
-       <Image src='https://bijouxhair.com/tim/landing2/3xghanaphoto2.jpg' alt='banner' width='800px' height='960px' className={cla.image}/>
-       <div className={cla.videotitle}> Try new style with 3X Ghana Braid  </div>
-       </div>
-     </div>
-       <div className={cla.album}>
-       <div  > 
-       <Image src='https://bijouxhair.com/tim/landing2/3xghanaphoto1.jpg' alt='banner' width='800px' height='900px' className={cla.image}/>
-       </div>
-     </div> */}
-       
+    
        <Image src='https://bijouxhair.com/tim/landing2/3xghanaspec.jpg' alt='banner' width='600px' height='200px' className={cla.image}/>
        
        
--- components/xpression_barbados.js
+++ components/xpression_barbados.js
@@ -1,54 +1,10 @@
 import React from 'react';
 import cla from '../styles/form.module.css';
-import {useRef, useState} from 'react';
-import {useRouter} from 'next/router'
 import Image from 'next/image';
 import Head from 'next/head'
 
 function xpression( ) {
-   
-    //  const refName = useRef();
-    //  const refTel = useRef();
-    //  const refEmail = useRef();
-    
-    //  const router = useRouter()
-    
-     
-    //  async function submitHandler(event) {
-    //    event.preventDefault();
-    //    const enteredName = refName.current.value;
-    //    const enteredTel = refTel.current.value;
-    //    const enteredEmail = refEmail.current.value;
-     
-      
-    //    const inputData ={
-    //      name: enteredName,
-    //      tel: enteredTel,
-    //      email: enteredEmail,
-       
-    //      date: Date(),
-    //    }
-    //    console.log(inputData);
-    //    addDataHandler(inputData)
-
-    //     document.getElementById('name').value='';
-    //     document.getElementById('tel').value='';
-    //     document.getElementById('email').value='';
-      
-    //  }
-    //  async function addDataHandler(inputData) {
-
-    //     const response = await fetch('/api/postsghana',
-    //         {
-    //             method: 'POST',
-    //             body: JSON.stringify(inputData),
-    //             headers: {
-    //                 'Content-type': 'application/json'
-    //             }
-    //         })
-    //     const data = await response.json();
-    //     router.push('/thanks')
-    // }
+  
   return (
     
     <div className={cla.main}> 
@@ -63,26 +19,7 @@ function xpression( ) {
         <meta property="og:site_name" content="Beauty Elements"/> 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head> 
-        {/* <center>
-        <Image src='https://bijouxhair.com/tim/landing2/xpressionhead.jpg' width='800px' height='500px' alt='banner' />
-        <div className={cla.ula2}>Fill out the Registration and Win !</div>
-      <div className={cla.inputframe}>
-      <form onSubmit={submitHandler}>
-         <input type='text' id='name' ref={refName} placeholder='Name' className={cla.inputbox} required /><br/>
-         <input type='text' id='tel' ref={refTel} placeholder='Tel' className={cla.inputbox} required /><br/>
-         <input type='text' id='email' ref={refEmail} placeholder='Email' className={cla.inputbox} required /> 
-         <button className={cla.btn}>Submit</button>
-       </form>
-       </div>
-       <div className={cla.textbox}>
-       <div className={cla.ula2}>To get the free giveaway Hair: </div>
-       <ul className={cla.ula}>
-         <li>  5 winners will be selected every 2 weeks.</li>
-         <li>  5 Packs 3x Ghana braid 50 for each Winner.</li>
-       </ul>
-       </div>
-       </center> */}
-     {/* <div className={cla.phototitle}>3X Ghana Braid Special Event</div> */}
+      
      <div className={cla.album2}>
        <div >
        <Image src='https://bijouxhair.com/tim/landing2/4xxpression_head.jpg' alt='banner' width='800px' height='400px' className={cla.image}/>
